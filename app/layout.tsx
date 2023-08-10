f3a0332c08daf6f2681e5bea6258411c7883e662
Colin C <me@thecolin.co>
2023-08-10 06:32:14 -0400

    Fixed rendering errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
+'use client';
+
 import React from 'react';
 import '@/app/_styles/globals.scss';
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 import { Work_Sans } from 'next/font/google';
-import { SkipNavContent } from '@reach/skip-nav';
+// import { SkipNavContent } from '@reach/skip-nav';
 import { Header } from '@/app/_components/header';
 import styles from './layout.module.scss';
 import { Footer } from '@/app/_components/footer';
 
 const workSans = Work_Sans({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'MITech Consulting Group',
-  description:
-    "MIT's newest consulting club that offers innovative solutions to businesses looking to take their operations to the next level.",
-  themeColor: '#9e3ffd',
-};
+// export const metadata: Metadata = {
+//   title: 'MITech Consulting Group',
+//   description:
+//     "MIT's newest consulting club that offers innovative solutions to businesses looking to take their operations to the next level.",
+//   themeColor: '#9e3ffd',
+// };
 
 export interface RootProps {
   children: React.ReactNode;
@@ -26,7 +28,7 @@ const RootLayout = ({ children }: RootProps) => {
       <body className={workSans.className}>
         <Header />
         <main className={styles.content}>
-          <SkipNavContent />
+          {/*<SkipNavContent />*/}
           {children}
         </main>
         <Footer />
