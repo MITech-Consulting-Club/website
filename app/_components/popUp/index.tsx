57a9ae1728b54ca0f669f8635057f23d5a7e775e
Nayeemur <nayeem3156@gmail.com>
2023-08-20 11:58:32 -0400

    ;)

--- app/_components/popUp/index.tsx
+++ app/_components/popUp/index.tsx
@@ -12,22 +12,29 @@ interface PopUpProps {
 
 const PopUp = ({ image, name, role, bio, onCloseProfile }: PopUpProps) => {
   return (
-    <div className={styles.card}>
-      <Image
-        src={`/about/execImages/${image}`}
-        alt="profile"
-        width={500}
-        height={500}
-        className={styles.profileImage}
-      />
-      <div className={styles.profileContainer}>
-        <p className={styles.profileName}>{name}</p>
-        <p className={styles.profileRole}>{role}</p>
-        <p className={styles.profileBio}>{bio}</p>
+    <div className={styles.popupContainer}>
+      <div className={styles.card}>
+        <Image
+          src={`/about/execImages/${image}`}
+          alt="profile"
+          width={500}
+          height={500}
+          className={styles.profileImage}
+        />
+        <div className={styles.profileContainer}>
+          <p className={styles.profileName}>{name}</p>
+          <p className={styles.profileRole}>{role}</p>
+          <p className={styles.profileBio}>{bio}</p>
+        </div>
+        <button className={styles.closeButton} onClick={onCloseProfile}>
+          x
+        </button>
       </div>
-      <button className={styles.closeButton} onClick={onCloseProfile}>
-        x
-      </button>
+      <div
+        className={`${
+          name == 'Nayeemur Rahman' ? styles.animationBackground : ''
+        }`}
+      ></div>
     </div>
   );
 };
